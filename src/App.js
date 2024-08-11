d47efb24fa8f6e900e1749ef2d593347484d3cde
Andrew Hughes <andrewhughes@Andrews-Laptop.local>
2024-08-11 14:42:42 -0500

    add css for initial components, create components for review section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import MovieList from './MovieList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <h1>Movie Reviews</h1>
+      <div className="container-fluid">
+        <h1 className="text-center mt-5 mb-5 fw-bold">Movie Reviews</h1>
+      </div>
       <MovieList />
     </div>
   );
--- src/Movie.js
+++ src/Movie.js
@@ -8,12 +8,33 @@ export default class Movie extends React.Component {
         const {movie} = this.props;
 
         return (
-            <div>
-                <h2>{movie.title}</h2>
-                <img src={movie.image} alt={movie.title} className="movieImg" />
-                <p>{movie.synopsis}</p>
-                <Stars name="Star Rating"/>
+            <div className="container d-flex justify-content-center">
+                <div className="card mb-5 w-75 align-items-center text-center">
+                <img className="card-img-top" src={movie.image} alt={movie.title} />
+                <div className="card-body">
+                    <h2 className="card-title">{movie.title}</h2>
+                    <p className="card-text">{movie.synopsis}</p>
+                    <Stars name="Star Rating"/>
+                </div>
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="col">
+                            <h1>Review List</h1>
+                        </div>
+                        <div className="col">
+                            <h1>Review List</h1>
+                        </div>
+                        <div className="col">
+                            <h1>Review List</h1>
+                        </div>
+                    </div>                    
+                </div>
+                <div className="card-footer w-100">
+                    <h1>Review Form</h1>
+                </div>
+                </div>
             </div>
+            
         )
     }
 }
